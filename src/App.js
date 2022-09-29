959d444519ad35e04c335cda8d9738ad25d130f3
Imon <contact@imonweb.co.uk>
2022-09-29 23:34:55 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box-component';
 import './App.css';
 
 const App = () => {
+  const [searchField, setSearchField] = useState('');
+  console.log({searchField});
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
 
   return (
      <div className='App'>
@@ -47,12 +53,12 @@ const App = () => {
 //       ))
 //   }
 
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   }
+  // onSearchChange = (event) => {
+  //   const searchField = event.target.value.toLocaleLowerCase();
+  //   this.setState(() => {
+  //     return { searchField };
+  //   });
+  // }
 
 //   render() {
 
